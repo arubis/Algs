64bd04719737b2061c54515f5cc95c0dc431ab78
Dylan Fitzgerald <dylan@dylanfitzgerald.net>
2014-02-22 10:11:21 -0700

    Run through FindBugs and CheckStyle

--- assignment-1_percolation/PercolationStats.java
+++ assignment-1_percolation/PercolationStats.java
@@ -1,18 +1,20 @@
 // please be gentle.
 
 public class PercolationStats {
-    int[] opened;
-    int total_opened;
-    double[] percThreshold;
-    int N, T;
+    private int[] opened;
+    private double[] percThreshold;
+    private int N, T;
     
-    public PercolationStats(int n, int t) {  // perform T independent computational experiments on an N-by-N grid
+    public PercolationStats(int n, int t) {  
+        // perform T independent computational experiments on an N-by-N grid
         // set these guys up throughout the class
-        N = n; T = t;
+        N = n; 
+        T = t;
         
         // get all pissy
-        if ( N <= 0 ) throw new IllegalArgumentException("Gotta getta grid");
-        if ( T <= 0 ) throw new IllegalArgumentException("If you don't wanna test get outta here!");
+        if (N <= 0) throw new IllegalArgumentException("Gotta getta grid");
+        if (T <= 0) throw new 
+            IllegalArgumentException("If you don't wanna test get outta here!");
         
         // then let's decide how to share
         opened = new int[T];
@@ -21,13 +23,13 @@ public class PercolationStats {
         int i;
         Percolation p;
         
-        for(i = 0; i < T; i++) {
+        for (i = 0; i < T; i++) {
             p = new Percolation(N);
             opened[i] = 0;
 
             int row = 1;
             int col = 1;
-            while( !p.percolates() ) {
+            while (!p.percolates()) {
                 row = StdRandom.uniform(N) + 1;
                 col = StdRandom.uniform(N) + 1;
                 if (!p.isOpen(row, col)) {
@@ -40,23 +42,26 @@ public class PercolationStats {
         }
     }
    
-    public double mean()  {                  // sample mean of percolation threshold
+    public double mean()  {              // sample mean of percolation threshold
         return StdStats.mean(percThreshold);
     }
    
-    public double stddev() {                 // sample standard deviation of percolation threshold
+    public double stddev() { // sample standard deviation of percolation threshold
         return StdStats.stddev(percThreshold);
     }
-    public double confidenceLo() {           // returns lower bound of the 95% confidence interval
-        return (mean() - ((1.96 * stddev()) / Math.sqrt((double) T))) ;
+    public double confidenceLo() { 
+        // returns lower bound of the 95% confidence interval
+        return (mean() - ((1.96 * stddev()) / Math.sqrt((double) T)));
     }
    
-    public double confidenceHi() {           // returns upper bound of the 95% confidence interval
+    public double confidenceHi() { 
+        // returns upper bound of the 95% confidence interval
         return (mean() + ((1.96 * stddev()) / Math.sqrt((double) T)));
     }
    
     public static void main(String[] args) { // test client, described below
-        if (args.length != 2) throw new IllegalArgumentException("Need exactly two numeric arguments");
+        if (args.length != 2) throw new 
+            IllegalArgumentException("Need exactly two numeric arguments");
         int t = Integer.parseInt(args[0]);
         int n = Integer.parseInt(args[1]);
         
