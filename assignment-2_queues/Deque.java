82d00f4aacbd26709413be1b3d1bf7c0e57d8ee9
Dylan Fitzgerald <dylan@dylanfitzgerald.net>
2014-03-01 15:04:41 -0700

    Pretend to use array (but we need linked list here)

--- assignment-2_queues/Deque.java
+++ assignment-2_queues/Deque.java
@@ -1,13 +1,23 @@
 import java.util.Iterator;
 
 public class Deque<Item> implements Iterable<Item> {
-   public Deque()                         {}  // construct an empty deque
-   public boolean isEmpty()               { return true; }  // is the deque empty?
-   public int size()                      { return 7; }  // return the number of items on the deque
-   public void addFirst(Item item)        {}  // insert the item at the front
-   public void addLast(Item item)         {}  // insert the item at the end
-   // public Item removeFirst()              { return ; }  // delete and return the item at the front
-   // public Item removeLast()               { return ; }  // delete and return the item at the end
-   // public Iterator<Item> iterator()       { return; }  // return an iterator over items in order from front to end
-   public static void main(String[] args) {}  // unit testing
+  public Deque()                         {}  // construct an empty deque
+  public boolean isEmpty()               { return true; }  // is the deque empty?
+  public int size()                      { return 7; }  // return the number of items on the deque
+  public void addFirst(Item item)        {}  // insert the item at the front
+  public void addLast(Item item)         {}  // insert the item at the end
+  // public Item removeFirst()              { return ; }  // delete and return the item at the front
+  // public Item removeLast()               { return ; }  // delete and return the item at the end
+  public Iterator<Item> iterator()        // return an iterator over items in order from front to end
+  { return new ForwardLLIterator(); }
+  private class ForwardLLIterator implements Iterator<Item>
+  {
+    private int i = 0;            // start at front of array (index 0)
+
+    public boolean hasNext()  { return i < N; }
+    public Item next()        { return ; }
+
+    public void remove()      { throw new UnsupportedOperationException("remove() is scary"); }
+  }
+  public static void main(String[] args) {}  // unit testing
 }
